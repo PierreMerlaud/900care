a4c67887265353c83011d2d0b08b63e5ca3090a7
pierre merlaud <pmerlaud007@gmail.com>
2023-11-04 13:07:57 +0100

    tried test but not working

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,11 +6,16 @@ import { Planete } from "./components/Planete/client";
 import { Abonnement } from "./components/Abonnement/client";
 import { Produits } from "./components/Produits/client";
 
+interface RootPageProps {
+  params: {
+    locale: string;
+  };
+}
 //Node.js 18.17
-export default function Home({ params: { locale } }) {
+export default function Home({ params: { locale } }: RootPageProps) {
   return (
     <main className="min-h-screen">
-      <div className="flex flex-col">
+      <div className="flex flex-col pb-20">
         <MobileHeader />
         <Hero lng={locale} />
         <Planete lng={locale} />
